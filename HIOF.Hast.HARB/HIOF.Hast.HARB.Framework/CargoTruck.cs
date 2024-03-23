28c6d5abd7e16214471ccc51c629cd1e3352c5da
Sindrebh <sindre.bless.horn@gmail.com>
2024-03-23 21:01:25 +0100

    klassene CargoTruck og Crane til cargo transport

---
+++ HIOF.Hast.HARB/HIOF.Hast.HARB.Framework/CargoTruck.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace HIOF.Hast.HARB.Framework
+{
+    public class CargoTruck
+    {
+        public int Id { get; }
+        public bool IsLoaded { get; private set; }
+        public Cargo? LoadedCargo { get; private set; }
+
+        public CargoTruck(int id)
+        {
+            Id = id;
+            IsLoaded = false;
+        }
+
+        public void LoadCargo(Cargo cargo)
+        {
+            if (!IsLoaded)
+            {
+                LoadedCargo = cargo;
+                IsLoaded = true;
+                Console.WriteLine($"Truck {Id} loaded with cargo {cargo.Id}");
+            }
+            else
+            {
+                Console.WriteLine($"Truck {Id} is already loaded with cargo.");
+            }
+        }
+
+        public void UnloadCargo()
+        {
+            if (IsLoaded)
+            {
+                Console.WriteLine($"Truck {Id} unloaded cargo.");
+                LoadedCargo = null;
+                IsLoaded = false;
+            }
+            else
+            {
+                Console.WriteLine($"Truck {Id} is not loaded with cargo.");
+            }
+        }
+    }
+}
+
---
+++ HIOF.Hast.HARB/HIOF.Hast.HARB.Framework/Crane.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIOF.Hast.HARB.Framework
+{
+    internal class Crane
+    {
+        internal int Id { get; private set; }
+        internal bool IsAvailable { get; private set; }
+
+        public Crane(int craneId)
+        {
+            Id = craneId;
+            IsAvailable = true; 
+        }
+
+        public bool AcquireCrane()
+        {
+            if (IsAvailable)
+            {
+                IsAvailable = false; 
+                return true; 
+            }
+            return false; 
+        }
+
+        public void ReleaseCrane()
+        {
+            IsAvailable = true;
+        }
+    }
+}
+
